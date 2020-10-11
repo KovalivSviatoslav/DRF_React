943187d1dd09ab17399a977a67133dc4f77195e1
Sviatoslav Kovaliv <kovaliv2195@gmail.com>
2020-10-12 00:09:15 +0300

    completed setup

---
+++ src/todo/urls.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+from .api import TodoViewSet
+
+routers = routers.DefaultRouter()
+routers.register('api/todo', TodoViewSet, 'todo')
+
+
+urlpatterns = routers.urls
